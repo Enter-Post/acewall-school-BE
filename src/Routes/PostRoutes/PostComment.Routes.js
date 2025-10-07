40d7a429098ecd91e2151b6494032aae3eb0eec6
talhahasif <imtalha995@gmail.com>
2025-10-08 01:15:33 +0500

    commnt get api

--- src/Routes/PostRoutes/PostComment.Routes.js
+++ src/Routes/PostRoutes/PostComment.Routes.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { sendPostComment } from "../../Contollers/PostControllers/postComment.controller.js";
+import { getPostComment, sendPostComment } from "../../Contollers/PostControllers/postComment.controller.js";
 import { isUser } from "../../middlewares/Auth.Middleware.js";
 
 const router = express.Router();
 
 router.post("/sendComment/:id", isUser, sendPostComment);
+router.get("/getPostComment/:id", isUser, getPostComment)
 
 export default router;
