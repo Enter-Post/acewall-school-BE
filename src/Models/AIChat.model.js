6b348ff7c15d1d38ab48eb103ce044fc0224728c
Talhahasif <imtalha995@gmail.com>
2025-12-10 11:30:16 -0500

    ai api

--- src/Models/AIChat.model.js
+++ src/Models/AIChat.model.js
@@ -2,8 +2,14 @@ import mongoose from "mongoose";
 
 const AIChatSchema = new mongoose.Schema({
     userId: String,
-    question: String,
-    answer: String,
+    question: {
+        text: String,
+        sender: String
+    },
+    answer: {
+        text: String,
+        sender: String
+    },
     difficulty: String,
     createdAt: { type: Date, default: Date.now },
 });
