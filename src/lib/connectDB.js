8273e894828af4dcd87149e9c8399b755bd5ff4f
Talhahasif <imtalha995@gmail.com>
2026-05-21 02:30:22 +0500

    ......................................

--- src/lib/connectDB.js
+++ src/lib/connectDB.js
@@ -2,9 +2,7 @@ import mongoose from "mongoose";
 
 export const connectDB = () => {
   try {
-    const connect = mongoose.connect(process.env.MONGODB_URI, {
-      dbName: "dev",
-    });
+    const connect = mongoose.connect(process.env.MONGODB_URI);
     console.log("DB connected succcesfully");
   } catch (error) {
     console.log("error in DB connection", error);
